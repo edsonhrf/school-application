d33843f16d41d5969e1cf4ce22a7bd23e7e69475
Athos Roberto de Souza Pereira Portes <athos.portes@meta.com.br>
2021-10-05 22:31:43 -0300

    new files

--- src/main/java/SchoolApplication.java
+++ src/main/java/SchoolApplication.java
@@ -1,11 +1,9 @@
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class SchoolApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(SchoolApplication.class, args);
     }
-
 }
